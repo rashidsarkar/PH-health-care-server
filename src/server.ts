c10bf65bde45bad039fb67c7cc07a88a590b01fe
rashidsarker <rashidrock558@gmail.com>
2025-04-08 20:22:54 +0600

    `Refactor server startup to use async main function`

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
+import { Server } from "http";
 import app from "./app";
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+async function main() {
+  const server: Server = app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+  });
+}
+
+main();
