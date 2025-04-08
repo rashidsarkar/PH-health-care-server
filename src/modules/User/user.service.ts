55b75b51f7e17c453aa5912e6c7f5053a31e73a1
rashidsarker <rashidrock558@gmail.com>
2025-04-09 00:46:59 +0600

    57.1 start

--- src/modules/User/user.service.ts
+++ src/modules/User/user.service.ts
@@ -1,7 +1,25 @@
-const createAdmin = async () => {
-  return {
-    message: "admin created",
+import { PrismaClient, UserRole } from "@prisma/client";
+import bcrypt from "bcrypt";
+const prisma = new PrismaClient();
+
+const createAdmin = async (data: any) => {
+  const hashedPassword = await bcrypt.hash(data.password, 12);
+
+  const userData = {
+    email: data.admin.email,
+    password: hashedPassword,
+    role: UserRole.ADMIN,
   };
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+    const createdAdminData = await transactionClient.admin.create({
+      data: data.admin,
+    });
+    return createdAdminData;
+  });
+  return result;
 };
 export const userService = {
   createAdmin,
