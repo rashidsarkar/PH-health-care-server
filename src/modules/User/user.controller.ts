236bcf059003151d456fac954564d7fc844724cf
rashidsarker <rashidrock558@gmail.com>
2025-04-10 23:21:36 +0600

    57.1 end

--- src/modules/User/user.controller.ts
+++ src/modules/User/user.controller.ts
@@ -2,8 +2,21 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userService.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await userService.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Internal Server Error",
+      error: error,
+    });
+  }
 };
 export const userController = {
   createAdmin,
