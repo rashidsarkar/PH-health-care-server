82952ec62823516b4fa0e55524cb14c1a466b621
rashidsarker <rashidrock558@gmail.com>
2025-04-11 12:27:22 +0600

    57.8 start

--- src/modules/Admin/admin.controller.ts
+++ src/modules/Admin/admin.controller.ts
@@ -1,18 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import { adminService } from "./admin.service";
+import { pick } from "../../shared/pick";
 
 const prisma = new PrismaClient();
 
 const getAllAdminFromDB = async (req: Request, res: Response) => {
   try {
-    const result = await adminService.getAllAdmin(req.query);
+    const filter = pick(req.query, [
+      "name",
+      "email",
+      "searchTerm",
+      "contactNumber",
+    ]);
+    const result = await adminService.getAllAdmin(filter);
     res.status(200).json({
       success: true,
       message: "Admins retrieved successfully",
       data: result,
     });
-  } catch (error) {
+  } catch (error: any) {
     res.status(500).json({
       success: false,
       message: error?.name || "Error occurred while retrieving admins",
--- src/modules/Admin/admin.service.ts
+++ src/modules/Admin/admin.service.ts
@@ -3,9 +3,11 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getAllAdmin = async (params: any) => {
+  const { searchTerm, ...filterData } = params;
   const andCondition: Prisma.AdminWhereInput[] = [];
 
   const adminSearchAbleFields = ["name", "email"];
+  // console.log(filterData);
   if (params?.searchTerm) {
     andCondition.push({
       OR: adminSearchAbleFields.map((field) => {
@@ -19,6 +21,18 @@ const getAllAdmin = async (params: any) => {
     });
   }
   // console.dir(andCondition, { depth: "infinity" });
+
+  if (Object.keys(filterData).length > 0) {
+    andCondition.push({
+      AND: Object.keys(filterData).map((key) => {
+        return {
+          [key]: {
+            equals: filterData[key],
+          },
+        };
+      }),
+    });
+  }
   const whereConditions: Prisma.AdminWhereInput = { AND: andCondition };
   const result = await prisma.admin.findMany({
     where: whereConditions,
