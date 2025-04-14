52bb63f11bc700873cc446ccc87bacd14b8bea1d
rashidsarker <rashidrock558@gmail.com>
2025-04-15 00:15:44 +0600

    5934 start

--- src/modules/Admin/admin.controller.ts
+++ src/modules/Admin/admin.controller.ts
@@ -1,136 +1,66 @@
-import { PrismaClient } from "@prisma/client";
-import { NextFunction, Request, Response } from "express";
+import { RequestHandler } from "express";
 import { adminService } from "./admin.service";
 import { pick } from "../../shared/pick";
 import { adminFilterableFields } from "./admin.const";
-import { handlePrismaError } from "../../utils/handlePrismaError";
 import { sendResponse } from "../../utils/sendResponse";
 import status from "http-status";
+import catchAsync from "../../shared/catchasync";
 
-const getAllAdminFromDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const filter = pick(req.query, adminFilterableFields);
-    const option = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
-    console.log(option);
-    console.log(filter);
-    const result = await adminService.getAllAdmin(filter, option);
-    sendResponse(res, {
-      statusCode: status.OK,
-      success: true,
-      message: "Admins retrieved successfully",
-      meta: result.meta,
-      data: result.data,
-    });
-  } catch (error: any) {
-    // handlePrismaError(error, res);
-    next(error);
-  }
-};
-const getAdminByid = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const id = req.params.id;
-    const result = await adminService.getAdminByIdFromDb(id);
-    // res.status(200).json({
-    //   success: true,
-    //   message: "Admin retrieved successfully",
-    //   data: result,
-    // });
-    sendResponse(res, {
-      statusCode: status.OK,
-      success: true,
-      message: "Admins retrieved successfully",
-      data: result,
-    });
-  } catch (error) {
-    // handlePrismaError(error, res);
-    next(error);
-  }
-};
-const updatedAdminByid = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const id = req.params.id;
 
-    const result = await adminService.updatedAdminByIdFromDb(id, req.body);
-
-    // res.status(200).json({
-    //   success: true,
-    //   message: "Admin Updated successfully",
-    //   data: result,
-    // });
-
-    sendResponse(res, {
-      statusCode: status.OK,
-      success: true,
-      message: "Admins Updated successfully",
-      data: result,
-    });
-  } catch (error) {
-    // handlePrismaError(error, res);
-    next(error);
-  }
-};
-const deleteAdminByid = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const id = req.params.id;
-    const result = await adminService.deleteAdminFromDb(id);
-    // res.status(200).json({
-    //   success: true,
-    //   message: "Admin deleted successfully",
-    //   data: result,
-    // });
-
-    sendResponse(res, {
-      statusCode: status.OK,
-      success: true,
-      message: "Admin deleted successfully",
-      data: result,
-    });
-  } catch (error) {
-    // handlePrismaError(error, res);
-    next(error);
-  }
-};
-const softDeleteAdminByid = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const id = req.params.id;
-    const result = await adminService.softDeleteAdminFromDb(id);
-    // res.status(200).json({
-    //   success: true,
-    //   message: "Admin deleted successfully",
-    //   data: result,
-    // });
-
-    sendResponse(res, {
-      statusCode: status.OK,
-      success: true,
-      message: "Admin deleted successfully",
-      data: result,
-    });
-  } catch (error) {
-    // handlePrismaError(error, res);
-    next(error);
-  }
-};
+const getAllAdminFromDB: RequestHandler = catchAsync(async (req, res) => {
+  const filter = pick(req.query, adminFilterableFields);
+  const option = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+  console.log(option);
+  console.log(filter);
+  const result = await adminService.getAllAdmin(filter, option);
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Admins retrieved successfully",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+const getAdminByid = catchAsync(async (req, res) => {
+  const id = req.params.id;
+  const result = await adminService.getAdminByIdFromDb(id);
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Admins retrieved successfully",
+    data: result,
+  });
+});
+const updatedAdminByid = catchAsync(async (req, res) => {
+  const id = req.params.id;
+  const result = await adminService.updatedAdminByIdFromDb(id, req.body);
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Admins Updated successfully",
+    data: result,
+  });
+});
+const deleteAdminByid = catchAsync(async (req, res) => {
+  const id = req.params.id;
+  const result = await adminService.deleteAdminFromDb(id);
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Admin deleted successfully",
+    data: result,
+  });
+});
+const softDeleteAdminByid = catchAsync(async (req, res) => {
+  const id = req.params.id;
+  const result = await adminService.softDeleteAdminFromDb(id);
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Admin deleted successfully",
+    data: result,
+  });
+});
 export const adminController = {
   getAllAdminFromDB,
   getAdminByid,
--- src/modules/Admin/admin.routes.ts
+++ src/modules/Admin/admin.routes.ts
@@ -1,11 +1,30 @@
 import { PrismaClient } from "@prisma/client";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { adminController } from "./admin.controller";
+import { AnyZodObject, z } from "zod";
 const router = express.Router();
 
+const update = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    contactNumber: z.string().optional(),
+  }),
+});
+const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+      });
+      return next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
 router.get("/", adminController.getAllAdminFromDB);
 router.get("/:id", adminController.getAdminByid);
-router.patch("/:id", adminController.updatedAdminByid);
+router.patch("/:id", validateRequest(update), adminController.updatedAdminByid);
 router.delete("/:id", adminController.deleteAdminByid);
 router.delete("/soft/:id", adminController.softDeleteAdminByid);
 export const adminRoutes = router;
