f5cec752002b3f38dbe8a6bf0e4229f84ce6e042
rashidsarker <rashidrock558@gmail.com>
2025-04-21 08:11:27 +0600

    `Update server log message to include 's'`

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import config from "./config";
 const port = config.port;
 async function main() {
   const server: Server = app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+    console.log(`server is running on port ${port} s`);
   });
 }
 
