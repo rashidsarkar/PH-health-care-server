affc85397aa1095982207dc28605b1723bc28997
rashidsarker <rashidrock558@gmail.com>
2025-04-23 23:28:59 +0600

    61.3 start

--- src/modules/User/user.routes.ts
+++ src/modules/User/user.routes.ts
@@ -1,5 +1,30 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
+import { jwtHelpers } from "../../helpers/jwtHelpers";
+import config from "../../config";
 const router = express.Router();
-router.post("/", userController.createAdmin);
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      // console.log(token);
+      if (!token) {
+        // return res.status(401).json({ message: "Unauthorized: No token provided" });
+        throw new Error("Unauthorized: Token not found");
+      }
+      const verifiedUser = jwtHelpers.verifyToken(
+        token,
+        config.jwt.access_token_secret as string
+      );
+      console.log(verifiedUser);
+      if (roles.length && !roles.includes(verifiedUser.role)) {
+        throw new Error("Unauthorized: User role not permitted");
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+router.post("/", auth("ADMIN", "super"), userController.createAdmin);
 export const userRoutes = router;
